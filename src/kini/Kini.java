f77eea454b364d63b0ae68563bf1bef116ec5bb5
Administrador <Administrador@FI-PC>
2019-06-12 18:13:38 -0300

    Vectores Ordenados

--- src/kini/Kini.java
+++ src/kini/Kini.java
@@ -33,77 +33,131 @@ public class Kini {
         int sorteo[] = new int [tam];
         
         
+        //Usuario Ingresa los numeros, programa valida que sean entre 0 y 45
+        
          System.out.println("Hola ahora ingresa tus numeros ");
-         
+        //Se pide Ingresar numero
         while (m<6){
-        System.out.println("Ingresa tu numero" + (m));
+        System.out.println("Ingresa tu numero" + (m+1));
         aux = entrada.nextInt();
+        //Se evalua que este entre 0 y 45
         if(aux>45||aux<0){   
         System.out.println("ERROR Recuerda que el numero debe estar entre 0 y 45");
         }
         if(aux<46&&aux>-1){
-        System.out.println("Hola");
-        arreglo[m]= aux;
-        m = m +1;
+        if (m>0){
+         n = k = 0;
+       // Se evalua que el numero ingresado no este en el arreglo
+          for(int j=0;j<m;j++){
+          if(aux==arreglo[j]){
+              k = k + 1;    
+          }
+          if(aux!=arreglo[j]){
+              k = k + 1;
+              n = n + 1;   
+          }
+          } 
+          //Si el numero ingresado no esta en el arreglo se agrega
+          if (n==k){
+          arreglo[m]= aux;
+          m = m+ 1;
+          }
+          if (n!=k){
+          System.out.println("El numero " + aux + " ya lo elegiste ");      
+          }
         }
+          if (m==0){
+            arreglo[m]= aux;
+          m = m+1;
+            }
+            }   
+        }    
+        
+        //Se Ordena arreglo ingresado por Usuario
+        for (int i=0; i<arreglo.length;i++){
+            
+            for (int j=i+1;j<arreglo.length;j++){
+                if (arreglo[i]>arreglo[j]){
+                    aux = arreglo[i];
+                    arreglo[i]=arreglo[j];
+                    arreglo[j]=aux;
+                } 
+            }
         }
         
-        
+
+// Imprime el arreglo ordenado 
+        System.out.println("Los numeros que elegiste fueron");
+        for (int i =0;i<tam;i++){
+     
+        System.out.println(arreglo[i]);         
+        }
+                
+                
        
+       
+
+        //Programa genera los aleatorios validando que no se repitan
        
        if (l==0){
        Random generadorAleatorios = new Random();
        int NumeroAleatorio = generadorAleatorios.nextInt(45);   
        sorteo[0]= NumeroAleatorio;
-       System.out.println("El numero "+ l + " es "+ sorteo[0]);
        l = l +1;
-
-        }
+       }
        
        while(l<6){
        Random generadorAleatorios = new Random();
        int NumeroAleatorio = generadorAleatorios.nextInt(45);
-       System.out.println("Genere el numero" + NumeroAleatorio);
        n = k = 0;
        
           for(int j=0;j<l;j++){
-          
           if(NumeroAleatorio==sorteo[j]){
-              
-              k = k + 1;
-          System.out.println("Aca llego");
-              
-              
+              k = k + 1;   
           }
           
           if(NumeroAleatorio!=sorteo[j]){
-              
               k = k + 1;
               n = n + 1;
-           System.out.println("Pase");
              
           }
-          
           }    
        
           if (n==k){
           sorteo[l]= NumeroAleatorio;
-          System.out.println("sorteo"+ l + " es " + NumeroAleatorio);
           l = l+ 1;
           }
+            } 
        
-       
-    } 
-       
-         for (int i =0;i<tam;i++){
+
      
-     System.out.println("El numero "+ i + " es "+ sorteo[i]); 
+   //Se Ordenan numeros de sorteo
+        for (int i=0; i<sorteo.length;i++){
+            
+            for (int j=i+1;j<sorteo.length;j++){
+                if (sorteo[i]>sorteo[j]){
+                    aux = sorteo[i];
+                    sorteo[i]=sorteo[j];
+                    sorteo[j]=aux;
+                } 
+            }
+        }
+        
+
+// Imprime el arreglo ordenado 
+        System.out.println("Los numeros sorteados son");
+        for (int i =0;i<tam;i++){
+     
+        System.out.println(sorteo[i]);         
+        }   
+     
+     
+     
        
 }
         
-    }}
+    }
+
      
-     
-         
         
     
